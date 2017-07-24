3a21402af11e968474a03795721ecd2140f22d51
MariKKKKKKK <mark.prnk@gmail.com>
2017-07-24 17:55:52 +0300

    Product's page grid works ok in all needed browsers.

--- productResp.php
+++ productResp.php
@@ -12,7 +12,7 @@ include("functions/functions.php");
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="css/prodR.css">
+    <link rel="stylesheet" href="css/productsPage.css">
 
 </head>
 
